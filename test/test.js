9ba6875259f9eee64232b98e0f153fd221e3152f
hij1nx <fernmicro@gmail.com>
2011-05-26 14:35:34 -0400

    fleshing out implementations

--- test/test.js
+++ test/test.js
@@ -3,8 +3,12 @@ var m = new Memvat;
 
 console.log('start')
 
-m.on('get', 'foo', function(key, value) {
-  console.log(key, value);
+// m.on('get', 'foo', function(key, value) {
+//   console.log(key, value);
+// });
+
+m.on('get', function(key, value) {
+  console.log('>>>'+key, value);
 });
 
 m.set('foo', 'bar');
